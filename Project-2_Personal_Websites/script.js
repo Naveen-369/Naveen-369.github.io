70947cf9cb70475b32d63d3fd198e25d77527465
Naveen-369 <nk349352@gmail.com>
2023-12-11 18:45:17 +0530

    First commit

---
+++ Project-2_Personal_Websites/script.js
@@ -0,0 +1,19 @@
+/*Declaration of the function for redirecting to the project 1 github link*/
+function redirect_Project_1(){
+    var websiteRedirect="https://github.com/Naveen-369/Websites/tree/Production_Server/Project1-Simple_Self_Websites";
+    window.open(websiteRedirect, "_blank");
+}/*Declaration of the function for redirecting to the project 2 github link */
+function redirect_Project_2() {
+    var WebsiteRedirect="https://github.com/Naveen-369/First/tree/main/Web_Developement";
+    window.open(WebsiteRedirect, "_blank");
+}
+/*Declaration of the function for redirecting to the project 3 github link*/
+function redirect_Project_3(){
+    var websiteRedirect="https://github.com/Naveen-369/Micro_Projects_Basics/tree/main/Python";
+    window.open(websiteRedirect, "_blank");
+}
+/*Declaration of the function for redirecting to the project  */
+function redirect_Project_4(){
+    var websiteRedirect="https://github.com/Naveen-369/Micro_Projects_Basics/tree/main/Java";
+    window.open(websiteRedirect, "_blank");
+}
