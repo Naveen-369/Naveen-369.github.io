272db1a70fa1869edf03258e4c72af3971674691
Naveen-369 <nk349352@gmail.com>
2023-12-24 10:11:12 +0530

    Reduced the funtion declaration size with the concept of arrow functions in javascript

--- Project-2_Personal_Websites/script.js
+++ Project-2_Personal_Websites/script.js
@@ -1,19 +1,11 @@
 /*Declaration of the function for redirecting to the project 1 github link*/
-function redirect_Project_1(){
-    var websiteRedirect="https://github.com/Naveen-369/Websites/tree/Production_Server/Project1-Simple_Self_Websites";
-    window.open(websiteRedirect, "_blank");
-}/*Declaration of the function for redirecting to the project 2 github link */
-function redirect_Project_2() {
-    var WebsiteRedirect="https://github.com/Naveen-369/First/tree/main/Web_Developement";
-    window.open(WebsiteRedirect, "_blank");
-}
+redirect_Project_1 = ()=> window.open("https://github.com/Naveen-369/Websites/tree/Production_Server/Project1-Simple_Self_Websites", "_blank");
+
+/*Declaration of the function for redirecting to the project 2 github link */
+redirect_Project_2 = ()=> window.open("https://github.com/Naveen-369/First/tree/main/Web_Developement", "_blank");
+
 /*Declaration of the function for redirecting to the project 3 github link*/
-function redirect_Project_3(){
-    var websiteRedirect="https://github.com/Naveen-369/Micro_Projects_Basics/tree/main/Python";
-    window.open(websiteRedirect, "_blank");
-}
+redirect_Project_3 = ()=> window.open("https://github.com/Naveen-369/Micro_Projects_Basics/tree/main/Python", "_blank");
+
 /*Declaration of the function for redirecting to the project 4 github link */
-function redirect_Project_4(){
-    var websiteRedirect="https://github.com/Naveen-369/Websites/tree/Production_Server/Project-2_Personal_Websites";
-    window.open(websiteRedirect, "_blank");
-}
+redirect_Project_4 = ()=> window.open("https://github.com/Naveen-369/Websites/tree/Production_Server/Project-2_Personal_Websites", "_blank");
