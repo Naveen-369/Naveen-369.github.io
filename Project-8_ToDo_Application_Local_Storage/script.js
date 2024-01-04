4bd9f1e7812bef2f8aac47cdfd29c92e5167f27a
Naveen-369 <nk349352@gmail.com>
2024-01-04 18:48:50 +0530

    Added the functionality of local storage access

--- Project-8_ToDo_Application_Local_Storage/script.js
+++ Project-8_ToDo_Application_Local_Storage/script.js
@@ -1,5 +1,16 @@
+//Check if the memory is stored in the local storage and insert it
+window.onload=()=>{
+    localStorage.getItem("tasks")?tasks=JSON.parse(localStorage.getItem("tasks")):tasks=[];
+    //Insert the local storage elements in the html page
+    tasks.forEach((task)=>{
+    let divdoc=document.createElement('p');
+    divdoc.textContent=task;
+    document.querySelector(".tasks").appendChild(divdoc);
+})
+}
 //Declaration fo the array
 let tasks = [];
+
 //Declaraion of the function for inserting tasks into the division
 document.addEventListener("DOMContentLoaded",()=>{
     //declaratin of the handlers
@@ -11,11 +22,14 @@ document.addEventListener("DOMContentLoaded",()=>{
     //Declaration fo the function for buttons
     addtasks=()=>{
     let message = input.value;
-    tasks.push(message);
-    let doc = document.createElement('p');
-    doc.textContent = message; // Use textContent instead of innerHTML for text content
-    output.appendChild(doc);
-    input.value = "";
+    if(message!="")
+    {    
+        tasks.push(message);
+        let doc = document.createElement('p');
+        doc.textContent = message; // Use textContent instead of innerHTML for text content
+        output.appendChild(doc);
+        localStorage.setItem("tasks",JSON.stringify(tasks));}
+        input.value = "";
     }
 
     // Attach click event listener to the button using JavaScript
@@ -35,13 +49,6 @@ document.addEventListener("DOMContentLoaded",()=>{
     //Declaration fo the function for buttons
     output.addEventListener("click", (e) => {
         if (e.target.tagName === "P") {
-            e.target.style.textDecoration = "line-through";
-            console.log(e.target.textContent);
-            remove(e.target.textContent);
-        }
-    });
-    output.addEventListener("dblclick", (e) => {
-        if (e.target.tagName === "P") {
             console.log(e.target.textContent);
             output.removeChild(e.target);
             remove(e.target.textContent);
@@ -51,5 +58,6 @@ document.addEventListener("DOMContentLoaded",()=>{
     remove=(message)=>{
         let num=tasks.indexOf(message);
         tasks.splice(num,1);
+        localStorage.setItem("tasks",JSON.stringify(tasks));
     }
 })
